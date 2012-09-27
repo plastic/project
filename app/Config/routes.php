864b68c0e4ed91146f069ca99c003c2c1b59751b
plastic <andre.seduc@gmail.com>
2012-09-27 17:56:26 -0300

    fix routes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -30,6 +30,7 @@
  * ...and connect the rest of 'Pages' controller's urls.
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	Router::connect('/admin', array('controller' => 'users', 'action' => 'login', 'admin' => true));
 
 /**
  * Load all plugin routes.  See the CakePlugin documentation on 
